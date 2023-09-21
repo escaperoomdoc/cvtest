00947c159c7a992de214014ea523b0b720c1d0b3
sergey <escaperoomdoc@gmail.com>
2023-09-21 13:18:30 +0300

    +

--- main.py
+++ main.py
@@ -8,22 +8,31 @@ import hashlib
 import uuid
 
 from dotenv import load_dotenv
+import numpy as np
 
 import cv2
 
-def go_blur():
-    img = cv2.imread('./temp/pic_1.jpeg', cv2.IMREAD_GRAYSCALE)
+def go_blur(img_name):
+    img = cv2.imread(img_name, cv2.IMREAD_GRAYSCALE)
     blur = cv2.blur(img,(5,5))
     cv2.imshow('blur', blur)
     cv2.waitKey(0)
 
 
-def go_canny():
-    img = cv2.imread('./temp/pic_1.jpeg', cv2.IMREAD_GRAYSCALE)
-    canny = cv2.Canny(img,100,200)
+def go_canny(img_name):
+    img = cv2.imread(img_name, cv2.IMREAD_GRAYSCALE)
+    canny = cv2.Canny(img,100,400)
     cv2.imshow('canny', canny)
     cv2.waitKey(0)
 
-go_blur()
-go_canny()
+def go_filter_1(img_name):
+    img = cv2.imread(img_name, cv2.IMREAD_GRAYSCALE)
+    kernel = np.ones((5,5),np.float32)/125
+    print(kernel)
+    dst = cv2.filter2D(img,-1,kernel)
+    cv2.imshow('go_filter_1', dst)
+    cv2.waitKey(0)
 
+# go_blur('./temp/pic_1.jpeg')
+# go_canny('./temp/pic_rzd_1.png')
+go_filter_1('./temp/pic_rzd_1.png')

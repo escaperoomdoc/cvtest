74c066ec9bf1c2ba83ec15f04a5fc8dd33c08b11
sergey <escaperoomdoc@gmail.com>
2023-09-21 11:53:37 +0300

    +

--- main.py
+++ main.py
@@ -9,3 +9,21 @@ import uuid
 
 from dotenv import load_dotenv
 
+import cv2
+
+def go_blur():
+    img = cv2.imread('./temp/pic_1.jpeg', cv2.IMREAD_GRAYSCALE)
+    blur = cv2.blur(img,(5,5))
+    cv2.imshow('blur', blur)
+    cv2.waitKey(0)
+
+
+def go_canny():
+    img = cv2.imread('./temp/pic_1.jpeg', cv2.IMREAD_GRAYSCALE)
+    canny = cv2.Canny(img,100,200)
+    cv2.imshow('canny', canny)
+    cv2.waitKey(0)
+
+go_blur()
+go_canny()
+
